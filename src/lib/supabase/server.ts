04eb098d2da2d51e0781929eaec172c49b5b77fe
siljehtangen <siljehtangen@outlook.com>
2025-12-01 20:07:12 +0100

    feat: add server to supabase

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,1 +1,18 @@
- 
+import { createServerClient } from '@supabase/ssr';
+import { PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY } from '$env/static/public';
+import type { RequestEvent } from '@sveltejs/kit';
+
+export function createClient(event: RequestEvent) {
+	return createServerClient(PUBLIC_SUPABASE_URL, PUBLIC_SUPABASE_ANON_KEY, {
+		cookies: {
+			getAll() {
+				return event.cookies.getAll();
+			},
+			setAll(cookiesToSet) {
+				cookiesToSet.forEach(({ name, value, options }) => {
+					event.cookies.set(name, value, { ...options, path: '/' });
+				});
+			}
+		}
+	});
+}
